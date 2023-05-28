61521e3e06ce6af9c2f795762affcc8ebdad9b28
illya15 <113913094+illya15@users.noreply.github.com>
2023-05-28 21:03:25 +0300

    hw4 task 3 7

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,28 +1,25 @@
-// import { useEffect } from "react";
-
-
+import { useEffect } from 'react';
 
 export function Modal({ closeModal, modalImgUrl }) {
-
-
-  // useEffect(() => {
-  //   window.addEventListener('keydown', handleKeyDown);
-  // }, []);
-
-
-  // useEffect(() => {
-    
-
-  //   window.removeEventListener('keydown', handleKeyDown);
-  // }, [handleKeyDown]);
-
-
-
   const handleKeyDown = e => {
     if (e.code === 'Escape') {
       closeModal();
     }
   };
+
+  useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.code === 'Escape') {
+        closeModal();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [closeModal]);
 
   const handleOverlayClick = e => {
     if (e.target === e.currentTarget) {
@@ -31,14 +28,10 @@ export function Modal({ closeModal, modalImgUrl }) {
   };
 
   return (
-    <div
-      className="Overlay"
-      onClick={handleOverlayClick || handleKeyDown}
-    >
+    <div className="Overlay" onClick={handleOverlayClick || handleKeyDown}>
       <div className="Modal">
         <img src={modalImgUrl} alt="pictures" />
       </div>
     </div>
   );
 }
-
